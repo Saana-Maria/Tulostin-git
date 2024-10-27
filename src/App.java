60576b79bcf497ffa0b19943a0e9c86b25472fa0
Saana-Maria <saana-maria.aijanen@student.hamk.fi>
2024-10-27 11:00:10 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        
+                System.out.println("Hei olen Tulostin-ohjelma");
+        
+                System.out. print("Ohjelman tekijä: "); 
+        
+                String tekija = "Saana-Maria";
+        
+                System.out.println(tekija);
+        
+                double luku1 = 5;
+                double luku2 = 2;
+        
+                System.out.println(luku1);
+        
+                System.out.println("Luku1-muuttujan arvo on " + luku1 + ".");
+                System.out.println("Luku2-muuttujan arvo on " + luku2 + ".");
+        
+                double tulo = luku1 * luku2;
+        
+                System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+        
+                double erotus = luku1 - luku2;
+                double summa = luku1 + luku2;
+                double jako = luku1 / luku2;
+        
+                System.out.println(luku1 + " - " + luku2 + " = " + erotus);
+                System.out.println(luku1 + " + " + luku2 + " = " + summa);
+                System.out.println(luku1 + " / " + luku2 + " = " + jako);
+            }}
+
+    
